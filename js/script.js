a952cc87d4561b0e142c66b75f4f900eefebd586
Ekaterina <katsar14@yahoo.com>
2018-12-28 00:10:40 +0300

    завершение верстки

--- js/script.js
+++ js/script.js
@@ -1,23 +1,50 @@
 'use strict';
 
-var sliderButtons = document.querySelectorAll('.slider__control');
-var slides = document.querySelectorAll('.slider__item');
+(function() {
+  var sliderButtons = document.querySelectorAll('.slider__control');
+  var slides = document.querySelectorAll('.slider__item');
 
-sliderButtons.forEach(function(btn) {
-  btn.addEventListener('click', function(evt) {
-    sliderButtons.forEach(function(button) {
-      if (button.id === evt.target.id) {
-        button.classList.add('slider__control--active');
-      } else if (button.classList.contains('slider__control--active')) {
-        button.classList.remove('slider__control--active');
-      }
+  for (var i = 0; i < sliderButtons.length; i++) {
+    sliderButtons[i].addEventListener('click', function(evt) {
+      document.body.className = 'bg-' + evt.target.id;
+      for (var i = 0; i < sliderButtons.length; i++) {
+        if (sliderButtons[i].id === evt.target.id) {
+          sliderButtons[i].classList.add('slider__control--active');
+        } else if (sliderButtons[i].classList.contains('slider__control--active')) {
+          sliderButtons[i].classList.remove('slider__control--active');
+        }
+      };
+      for (var i = 0; i < slides.length; i++) {
+        if (slides[i].classList.contains('slider__item--' + evt.target.id)) {
+          slides[i].classList.remove('slider__item--hidden')
+        } else {
+          slides[i].classList.add('slider__item--hidden')
+        }
+      };
     });
-    slides.forEach(function(slide) {
-      if (slide.classList.contains('slider__item--' + evt.target.id)) {
-        slide.classList.remove('slider__item--hidden');
-      } else {
-        slide.classList.add('slider__item--hidden');
-      }
-    });
+  };
+})();
+
+(function() {
+  var modal = document.querySelector('.overlay');
+  var modalOpenBtn = document.querySelector('.contacts__btn');
+  var modalCloseBtn = modal.querySelector('.feedback__close-btn');
+
+  modalOpenBtn.addEventListener('click', function (evt) {
+    evt.preventDefault();
+    openModal();
   });
-});
+
+  var openModal = function () {
+    if(modal.classList.contains('overlay--hidden')) {
+      modal.classList.remove('overlay--hidden');
+    }
+
+    var onCloseBtnClick = function () {
+      modal.classList.add('overlay--hidden');
+      modalCloseBtn.removeEventListener('click', onCloseBtnClick);
+    }
+
+    modalCloseBtn.addEventListener('click', onCloseBtnClick);
+  }
+})();
